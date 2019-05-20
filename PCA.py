a8e58625214da4e681a4b12f906efb996f16f019
marinanenart <50808837+marinanenart@users.noreply.github.com>
2019-05-20 13:24:44 +0300

    Create PCA.py

---
+++ PCA.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_mldata
+from sklearn.decomposition import PCA
+import os
+import pandas as pd
+
+#соединение файлов в одну таблицу
+flag = 0
+for filename in os.listdir('files/'):
+    print(filename)
+    if flag == 0:
+        flag = 1
+        res = pd.read_csv('files/'+str(filename), sep='\t')
+    else:
+        ad = pd.read_csv('files/'+str(filename), sep='\t')
+        res = res.join(ad.set_index('Genes'), on='Genes')
+ 
+#транспонирование для PCA
+res = res.transpose()
+res = res[1:]
+
+#удаляем Nan
+res = res.loc[:, :34013]
+
+#PCA
+pca = PCA(n_components=40)
+pca_result = pca.fit_transform(res)
+pca_result = pd.DataFrame(pca_result)
+
+#для графиков, добавляем колонку с cells
+ct = ['cells' for i in range(len(list(res[list(res.columns)[0]])))]
+pca_result['cell_type'] = ct
+
+#запись результата в файл
+pca_result.to_csv('PCA_res.csv', index=False, header=True)
